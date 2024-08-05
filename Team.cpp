990a74df7767197285b1c7b9c3fbdc2d9cd4d771
Mritunjay021 <mrityurk21@gmail.com>
2024-08-05 22:12:34 +0530

    cpp

---
+++ Team.cpp
@@ -0,0 +1,31 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n,sum;
+    int c=0;
+    cin>>n;
+    int arr[n][3];
+    for(int i=0;i<n;i++)
+    {  
+        for(int j=0;j<3;j++)
+        {
+            cin>>arr[i][j];
+        }
+    }
+    for(int i=0;i<n;i++)
+    {
+        sum=0;
+        for(int j=0;j<3;j++)
+        {
+            sum+=arr[i][j];
+        }
+        if(sum>=2)
+        {
+            c++;
+        }
+    }
+    cout<<c;
+    return 0;
+}
---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,13 @@
+/*#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    string s=to_string(n);
+    string ns;
+    for(int i=s.length();i>=0;i--)
+    ns+=s[i];
+    cout<<(s+ns);
+    return 0;
+}*/
